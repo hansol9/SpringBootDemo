c3c1c5c9577494dfc924a412bc705215a8093888
hansol9 <hansol9@gmail.com>
2022-08-24 15:54:48 -0400

    server configuration for spring boot

--- src/main/java/hansol9/boot/SpringBootDemoApplication.java
+++ src/main/java/hansol9/boot/SpringBootDemoApplication.java
@@ -1,13 +1,44 @@
 package hansol9.boot;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
+
+    /*
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+
+        return connector;
+    }
+    */
 
 }
